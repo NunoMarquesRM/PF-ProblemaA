e2df8e48d8d5e3973d823d5643bd5805e17a6607
Nuno Marques <nuno_marques__1@hotmail.com>
2021-03-02 15:05:42 +0000

    Patch 4.0
    Ocaml: Modo iterativo done!!

--- A.ml
+++ A.ml
@@ -19,6 +19,8 @@ let m = ref 0;;
 let numFinal = ref 0;;
 let aux1 = ref 0;;
 let aux2 = ref 0;;
+let flag = ref true;;
+let regra = ref 0;;
 
 (* Retirado de:
 
@@ -38,35 +40,55 @@ let digits n =
 let () =
     let n = Scanf.scanf " %d" (fun x -> x) in
     numFinal := n;
-
-    (* Se for multiplo de 5  *)
-    if((n mod 5) = 0) then(
-        printf "Regra 3\n";
-        count := !count + 1;
-        m := 42;
-        numFinal:= n - !m
-    )
-    (* Se for par  *)
-    else if((n mod 2) = 0) then(
-        printf "Regra 1\n";
-        count := !count + 1;
-        m := n/2;
-        numFinal := !numFinal - !m
-        (* if(numFinal < 42) *)
-    )
-    else if((n mod 3) = 0 || (n mod 4) = 0) then(
-        printf "Regra 2\n";
-        count := !count + 1;
-        aux1 := !numFinal mod 10;
-        aux2 := (!numFinal - !aux1)/10;
-        m := !aux1 * !aux2;
-        numFinal := !numFinal - !m
-    )
-    else
-        printf "CRL\n";
     
+    while !flag do
+        m := 0;
+        if(!numFinal < 43) then(
+            flag := false;
+        )
+        (* Se for multiplo de 5  *)
+        else if((!numFinal mod 5) = 0) then(
+            printf "Regra 3\n";
+            count := !count + 1;
+            m := 42
+        )
+        (* Se for par  *)
+        else if((!numFinal mod 2) = 0) then(
+            count := !count + 1;
+            m := !numFinal/2;
+            regra := 1;
+            if((!numFinal - !m) < 42) then(
+                aux1 := !numFinal mod 10;
+                aux2 := (!numFinal - !aux1)/10;
+                m := !aux1 * !aux2;
+                regra := 2
+            );
+            printf "Regra %d\n" !regra;
+        )
+        else if((!numFinal mod 3) = 0 || (n mod 4) = 0) then(
+            count := !count + 1;
+            aux1 := !numFinal mod 10;
+            aux2 := (!numFinal - !aux1)/10;
+            m := !aux1 * !aux2;
+            regra := 2;
+            if((!numFinal - !m) < 42) then(
+                m := !numFinal/2;
+                regra := 1;
+            );
+            printf "Regra %d\n" !regra;
+        )
+        else(
+            (*printf "CRL\n";*)
+            flag := false;
+        );
+        numFinal:= !numFinal - !m;
+        printf "%d\n" !numFinal
+    done;
 
-    printf "Numero: %d\nCount: %d\n" !numFinal !count
-
-
-
+    if(!numFinal <> 42) then(
+        flag := false;
+        printf "BAD LUCK\n"
+    )
+    else(
+        printf "Numero: %d\nCount: %d\n" !numFinal !count
+    )
