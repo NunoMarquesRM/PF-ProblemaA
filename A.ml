0d5b488f60b68b3f0d6cc3222340d17d384d7506
Nuno Marques <nuno_marques__1@hotmail.com>
2021-03-03 13:02:40 +0000

    Patch 4.2
    Modo recursivo

--- A.ml
+++ A.ml
@@ -1,19 +1,5 @@
 open Printf
 
-(*  
-let rec game(n,count) = 
-    if((n mod 5) == 0) then
-        begin
-            (* printf("Regra 3"); *)
-            m = 42;
-            count := !count + 1
-            game(n-m,count)
-        end
-    
-    if((n mod 2) == 0) then
-        printf "Entrou\n";
-    
-*)
 let count = ref 0;;
 let m = ref 0;;
 let numFinal = ref 0;;
@@ -37,9 +23,65 @@ let digits n =
     | _ -> loop n []
 *)
 
+(*  *)
+let rec game(numFinal,count) = 
+    m := 0;
+    if(!numFinal > 42) then(
+        (* Se for multiplo de 5  *)
+        if((!numFinal mod 5) = 0) then(
+            (*printf "Regra 3\n";*)
+            count := !count + 1;
+            m := 42;
+            numFinal:= !numFinal - !m;
+            game(numFinal, count)
+        )
+        (* Se for par (multiplo de 2) *)
+        else if((!numFinal mod 2) = 0) then(
+            count := !count + 1;
+            m := !numFinal/2;
+            regra := 1;
+            if((!numFinal - !m) < 42) then(
+                aux1 := !numFinal mod 10;
+                aux2 := (!numFinal - !aux1)/10;
+                m := !aux1 * !aux2;
+                regra := 2
+            );
+            (*printf "Regra %d\n" !regra;*)
+            numFinal:= !numFinal - !m;
+            game(numFinal, count)
+        )
+        (* Se for multiplo de 3 ou multiplo de 4 *)
+        else if((!numFinal mod 3) = 0 || (!numFinal mod 4) = 0) then(
+            count := !count + 1;
+            aux1 := !numFinal mod 10;
+            aux2 := (!numFinal - !aux1)/10;
+            m := !aux1 * !aux2;
+            regra := 2;
+            if((!numFinal - !m) < 42) then(
+                m := !numFinal/2;
+                regra := 1;
+            );
+            (*printf "Regra %d\n" !regra;*)
+            numFinal:= !numFinal - !m;
+            game(numFinal, count)
+        );
+    );
+;;
+
 let () =
     let n = Scanf.scanf " %d" (fun x -> x) in
     numFinal := n;
+    if(!numFinal >= 0 && !numFinal <= 1000000) then(
+        game(numFinal,count)
+    );
+    if(!numFinal <> 42) then(
+        printf "BAD LUCK\n"
+    )
+    else(
+        (*printf "Numero: %d\nCount: %d\n" !numFinal !count*)
+        printf "%d\n" !count
+    )
+    (*
     if(!numFinal >= 0 && !numFinal <= 1000000) then(
         while !flag do
             m := 0;
@@ -52,7 +94,7 @@ let () =
                 count := !count + 1;
                 m := 42
             )
-            (* Se for par  *)
+            (* Se for par (multiplo de 2) *)
             else if((!numFinal mod 2) = 0) then(
                 count := !count + 1;
                 m := !numFinal/2;
@@ -65,7 +107,8 @@ let () =
                 );
                 printf "Regra %d\n" !regra;
             )
-            else if((!numFinal mod 3) = 0 || (n mod 4) = 0) then(
+            (* Se for multiplo de 3 ou multiplo de 4 *)
+            else if((!numFinal mod 3) = 0 || (!numFinal mod 4) = 0) then(
                 count := !count + 1;
                 aux1 := !numFinal mod 10;
                 aux2 := (!numFinal - !aux1)/10;
@@ -91,4 +134,4 @@ let () =
     )
     else(
         printf "Numero: %d\nCount: %d\n" !numFinal !count
-    )
+    )*)
