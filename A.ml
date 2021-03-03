ef15ffa9b7b9300b575ec9de0c0f4c7ea350c4a1
Nuno Marques <nuno_marques__1@hotmail.com>
2021-03-03 12:32:14 +0000

    Patch 4.1
    N entre 0 e 1 000 000

--- A.ml
+++ A.ml
@@ -40,53 +40,53 @@ let digits n =
 let () =
     let n = Scanf.scanf " %d" (fun x -> x) in
     numFinal := n;
-    
-    while !flag do
-        m := 0;
-        if(!numFinal < 43) then(
-            flag := false;
-        )
-        (* Se for multiplo de 5  *)
-        else if((!numFinal mod 5) = 0) then(
-            printf "Regra 3\n";
-            count := !count + 1;
-            m := 42
-        )
-        (* Se for par  *)
-        else if((!numFinal mod 2) = 0) then(
-            count := !count + 1;
-            m := !numFinal/2;
-            regra := 1;
-            if((!numFinal - !m) < 42) then(
+    if(!numFinal >= 0 && !numFinal <= 1000000) then(
+        while !flag do
+            m := 0;
+            if(!numFinal < 43) then(
+                flag := false;
+            )
+            (* Se for multiplo de 5  *)
+            else if((!numFinal mod 5) = 0) then(
+                printf "Regra 3\n";
+                count := !count + 1;
+                m := 42
+            )
+            (* Se for par  *)
+            else if((!numFinal mod 2) = 0) then(
+                count := !count + 1;
+                m := !numFinal/2;
+                regra := 1;
+                if((!numFinal - !m) < 42) then(
+                    aux1 := !numFinal mod 10;
+                    aux2 := (!numFinal - !aux1)/10;
+                    m := !aux1 * !aux2;
+                    regra := 2
+                );
+                printf "Regra %d\n" !regra;
+            )
+            else if((!numFinal mod 3) = 0 || (n mod 4) = 0) then(
+                count := !count + 1;
                 aux1 := !numFinal mod 10;
                 aux2 := (!numFinal - !aux1)/10;
                 m := !aux1 * !aux2;
-                regra := 2
+                regra := 2;
+                if((!numFinal - !m) < 42) then(
+                    m := !numFinal/2;
+                    regra := 1;
+                );
+                printf "Regra %d\n" !regra;
+            )
+            else(
+                (*printf "CRL\n";*)
+                flag := false;
             );
-            printf "Regra %d\n" !regra;
-        )
-        else if((!numFinal mod 3) = 0 || (n mod 4) = 0) then(
-            count := !count + 1;
-            aux1 := !numFinal mod 10;
-            aux2 := (!numFinal - !aux1)/10;
-            m := !aux1 * !aux2;
-            regra := 2;
-            if((!numFinal - !m) < 42) then(
-                m := !numFinal/2;
-                regra := 1;
-            );
-            printf "Regra %d\n" !regra;
-        )
-        else(
-            (*printf "CRL\n";*)
-            flag := false;
-        );
-        numFinal:= !numFinal - !m;
-        printf "%d\n" !numFinal
-    done;
+            numFinal:= !numFinal - !m;
+            printf "%d\n" !numFinal
+        done
+    );
 
     if(!numFinal <> 42) then(
-        flag := false;
         printf "BAD LUCK\n"
     )
     else(
