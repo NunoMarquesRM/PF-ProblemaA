4b67e45e69dc02d1f4cdaab24110f0c6cdb5afd5
Nuno Marques <nuno_marques__1@hotmail.com>
2021-03-03 15:05:20 +0000

    Update1

--- A.ml
+++ A.ml
@@ -14,40 +14,37 @@ let rec game(numFinal,count) =
     if(!numFinal > 42) then(
         (* Se for multiplo de 5  *)
         if((!numFinal mod 5) = 0) then(
-            (*printf "Regra 3\n";*)
+            (* *)printf "Regra 3\n";
             count := !count + 1;
             m := 42;
             numFinal:= !numFinal - !m;
-            game(numFinal, count)
-        )
-        (* Se for par (multiplo de 2) *)
-        else if((!numFinal mod 2) = 0) then(
-            count := !count + 1;
-            m := !numFinal/2;
-            regra := 1;
-            if((!numFinal - !m) < 42) then(
-                aux1 := !numFinal mod 10;
-                aux2 := (!numFinal - !aux1)/10;
-                m := !aux1 * !aux2;
-                regra := 2
-            );
-            (*printf "Regra %d\n" !regra;*)
-            numFinal:= !numFinal - !m;
+            (* *)printf "%d\n" !numFinal;
             game(numFinal, count)
         )
         (* Se for multiplo de 3 ou multiplo de 4 *)
         else if((!numFinal mod 3) = 0 || (!numFinal mod 4) = 0) then(
             count := !count + 1;
             aux1 := !numFinal mod 10;
-            aux2 := (!numFinal - !aux1)/10;
+            aux2 := ((!numFinal - !aux1) mod 100) / 10;
             m := !aux1 * !aux2;
             regra := 2;
-            if((!numFinal - !m) < 42) then(
+            if(!m = 0) then(
                 m := !numFinal/2;
                 regra := 1;
+                (* *)printf "-1-";
             );
-            (*printf "Regra %d\n" !regra;*)
+            (* *)printf "Regra %d\n" !regra;
             numFinal:= !numFinal - !m;
+            (* *)printf "%d\n" !numFinal;
+            game(numFinal, count)
+        )
+        (* Se for par (multiplo de 2) *)
+        else if((!numFinal mod 2) = 0) then(
+            count := !count + 1;
+            m := !numFinal/2;
+            (* *)printf "Regra 1\n";
+            numFinal:= !numFinal - !m;
+            (* *)printf "%d\n" !numFinal;
             game(numFinal, count)
         );
     );
@@ -63,8 +60,8 @@ let () =
         printf "BAD LUCK\n"
     )
     else(
-        (*printf "Numero: %d\nCount: %d\n" !numFinal !count*)
-        printf "%d\n" !count
+        (* *)printf "Numero: %d\nCount: %d\n" !numFinal !count
+        (* printf "%d\n" !count*)
     )
     (*
     if(!numFinal >= 0 && !numFinal <= 1000000) then(
