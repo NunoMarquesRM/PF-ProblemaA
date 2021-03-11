cb05d81e4b22d55f5a53480966a2beb0e3826666
Nuno Marques <nuno_marques__1@hotmail.com>
2021-03-11 12:40:10 +0000

    Update2

--- A.ml
+++ A.ml
@@ -5,7 +5,6 @@ let m = ref 0;;
 let numFinal = ref 0;;
 let aux1 = ref 0;;
 let aux2 = ref 0;;
-let flag = ref true;;
 let regra = ref 0;;
 
 (*  *)
@@ -14,11 +13,11 @@ let rec game(numFinal,count) =
     if(!numFinal > 42) then(
         (* Se for multiplo de 5  *)
         if((!numFinal mod 5) = 0) then(
-            (* *)printf "Regra 3\n";
             count := !count + 1;
             m := 42;
             numFinal:= !numFinal - !m;
-            (* *)printf "%d\n" !numFinal;
+            (* *)printf "Regra 3\n"; 
+            (* *)printf "%d\n" !numFinal; 
             game(numFinal, count)
         )
         (* Se for multiplo de 3 ou multiplo de 4 *)
@@ -27,24 +26,24 @@ let rec game(numFinal,count) =
             aux1 := !numFinal mod 10;
             aux2 := ((!numFinal - !aux1) mod 100) / 10;
             m := !aux1 * !aux2;
-            regra := 2;
+            (* *)regra := 2; 
             if(!m = 0) then(
                 m := !numFinal/2;
-                regra := 1;
+                (* *)regra := 1; 
                 (* *)printf "-1-";
             );
-            (* *)printf "Regra %d\n" !regra;
             numFinal:= !numFinal - !m;
-            (* *)printf "%d\n" !numFinal;
+            (* *)printf "Regra %d\n" !regra; 
+            (* *)printf "%d\n" !numFinal; 
             game(numFinal, count)
         )
         (* Se for par (multiplo de 2) *)
         else if((!numFinal mod 2) = 0) then(
             count := !count + 1;
             m := !numFinal/2;
-            (* *)printf "Regra 1\n";
             numFinal:= !numFinal - !m;
-            (* *)printf "%d\n" !numFinal;
+            (* *)printf "Regra 1\n"; 
+            (* *)printf "%d\n" !numFinal; 
             game(numFinal, count)
         );
     );
@@ -53,6 +52,9 @@ let rec game(numFinal,count) =
 let () =
     let n = Scanf.scanf " %d" (fun x -> x) in
     numFinal := n;
+    (*if(!numFinal = 42) then (
+        count := !count + 1
+    );*)
     if(!numFinal >= 0 && !numFinal <= 1000000) then(
         game(numFinal,count)
     );
@@ -60,60 +62,6 @@ let () =
         printf "BAD LUCK\n"
     )
     else(
-        (* *)printf "Numero: %d\nCount: %d\n" !numFinal !count
+        (* *)printf "%d\n" !count
         (* printf "%d\n" !count*)
     )
-    (*
-    if(!numFinal >= 0 && !numFinal <= 1000000) then(
-        while !flag do
-            m := 0;
-            if(!numFinal < 43) then(
-                flag := false;
-            )
-            (* Se for multiplo de 5  *)
-            else if((!numFinal mod 5) = 0) then(
-                printf "Regra 3\n";
-                count := !count + 1;
-                m := 42
-            )
-            (* Se for par (multiplo de 2) *)
-            else if((!numFinal mod 2) = 0) then(
-                count := !count + 1;
-                m := !numFinal/2;
-                regra := 1;
-                if((!numFinal - !m) < 42) then(
-                    aux1 := !numFinal mod 10;
-                    aux2 := (!numFinal - !aux1)/10;
-                    m := !aux1 * !aux2;
-                    regra := 2
-                );
-                printf "Regra %d\n" !regra;
-            )
-            (* Se for multiplo de 3 ou multiplo de 4 *)
-            else if((!numFinal mod 3) = 0 || (!numFinal mod 4) = 0) then(
-                count := !count + 1;
-                aux1 := !numFinal mod 10;
-                aux2 := (!numFinal - !aux1)/10;
-                m := !aux1 * !aux2;
-                regra := 2;
-                if((!numFinal - !m) < 42) then(
-                    m := !numFinal/2;
-                    regra := 1;
-                );
-                printf "Regra %d\n" !regra;
-            )
-            else(
-                (*printf "Entrou\n";*)
-                flag := false;
-            );
-            numFinal:= !numFinal - !m;
-            printf "%d\n" !numFinal
-        done
-    );
-
-    if(!numFinal <> 42) then(
-        printf "BAD LUCK\n"
-    )
-    else(
-        printf "Numero: %d\nCount: %d\n" !numFinal !count
-    )*)
--- A.py
+++ A.py
@@ -5,6 +5,7 @@ def function(N,i):
     global num, itera
     m = 0
     print(N)
+
     if(N <= 42):
         num = N
         itera = i
@@ -16,49 +17,38 @@ def function(N,i):
         i += 1
         function(N-m,i)
     
-    elif(N % 2 == 0):
-        m = int(N/2)
+    elif(N % 3 == 0 or N % 4 == 0):
+        aux1 = int(N % 10)
+        aux2 = int(((N-aux1) % 100) / 10)
+        m = int(aux1*aux2)
         i += 1
-        regra = 1
-        
-        if(N-m < 42):
-            aux1 = int(N / 10)
-            aux2 = int(N-(aux1*10))
-            m = int(aux1*aux2)
-            regra = 2
+        regra = 2
+
+        if(m == 0):
+            m = int(N/2)
+            regra = 1.0
         
         print("Regra "+str(regra))
         function(N-m,i)
     
-    elif(N % 3 == 0 or N % 4 == 0):
-        # Ex: m = 23
-        # aux1 = 2
-        # aux2 = 23 - 20 = 3
-        aux1 = int(N / 10)
-        aux2 = int(N-(aux1*10))
-        m = int(aux1*aux2)
+    elif(N % 2 == 0):
+        m = int(N/2)
         i += 1
-        
-        regra = 2
-
-        if(N-m < 42):
-            m = N/2
-            regra = 1
-        
+        regra = 1
         print("Regra "+str(regra))
-        function(N-m,itera)
+        function(N-m,i)
 
 
 print("A resposta e 42!\n")
 print("Introduza o valor: ")
 num = int(input())
 print("")
-m = num
+numInicial = num
 itera = 0
 
 function(num,itera)
 
 if(num == 42):
     print(itera)
-if(num < 42 or m == 0):
+else:
     print("BAD LUCK")
